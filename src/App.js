e6a66435f6e483e9a4e9829242a8f700c35f704f
tetianaShev2 <105647410+tetianaShev2@users.noreply.github.com>
2022-11-06 21:15:33 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather App</header>
-      <div vlassName="Footer">
-        <p>
-          This project was coded by Tetiana Shevchuk and is {""}
-          <a
-            href="https://github.com/tetianaShev2/weather-app-react"
-            target="_blank"
-          >
-            open-sourced on GitHub
-          </a>
-        </p>
+      <div className="container">
+        <header className="App-header">Weather App</header>
+        <button className="btn btn-primary"> click </button>
+        <div vlassName="Footer">
+          <p>
+            This project was coded by Tetiana Shevchuk and is {""}
+            <a
+              href="https://github.com/tetianaShev2/weather-app-react"
+              target="_blank"
+            >
+              open-sourced on GitHub
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
