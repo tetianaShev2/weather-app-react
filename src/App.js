ff3fc30642eccde0d747fb8b1b0881d66337381a
tetianaShev2 <105647410+tetianaShev2@users.noreply.github.com>
2022-11-06 15:48:51 +0100

    added footer link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Weather App</header>
+      <div vlassName="Footer">
+        <p>
+          This project was coded by Tetiana Shevchuk and is {""}
+          <a
+            href="https://github.com/tetianaShev2/weather-app-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
