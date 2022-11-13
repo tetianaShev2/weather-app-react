cc367923a50d6af56df0253079aad4a2614e399a
tetianaShev2 <105647410+tetianaShev2@users.noreply.github.com>
2022-11-13 16:11:41 +0100

    changed icon url

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: "http://openweathermap.org/img/wn/01d@2x.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
 
     setReady(true);
